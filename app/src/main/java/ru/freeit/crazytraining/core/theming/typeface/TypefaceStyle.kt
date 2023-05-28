35febb733cec2e8a720fb03dbdcd1b37121ec6f5
evitwilly <dmitry.kind.2@gmail.com>
2023-05-28 12:54:23 +0700

    changed font family

--- app/src/main/java/ru/freeit/crazytraining/core/theming/typeface/TypefaceStyle.kt
+++ app/src/main/java/ru/freeit/crazytraining/core/theming/typeface/TypefaceStyle.kt
@@ -1,9 +1,9 @@
 package ru.freeit.crazytraining.core.theming.typeface
 
 enum class TypefaceStyle(val typefaceManagerPath: String) {
-    LIGHT("sf_pro_display_bold.ttf"),
-    REGULAR("sf_pro_display_regular.ttf"),
-    MEDIUM("sf_pro_display_medium.ttf"),
-    SEMI_BOLD("sf_pro_display_semibold.ttf"),
-    BOLD("sf_pro_display_bold.ttf")
+    LIGHT("sf_pro_rounded_light.ttf"),
+    REGULAR("sf_pro_rounded_regular.ttf"),
+    MEDIUM("sf_pro_rounded_medium.ttf"),
+    SEMI_BOLD("sf_pro_rounded_semibold.ttf"),
+    BOLD("sf_pro_rounded_bold.ttf")
 }
