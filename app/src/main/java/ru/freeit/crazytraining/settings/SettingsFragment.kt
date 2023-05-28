479a6fcbc269b949bac5c662e635b66503100a57
evitwilly <dmitry.kind.2@gmail.com>
2023-05-28 11:50:03 +0700

    added CaptionTextView and changed Toolbar params

--- app/src/main/java/ru/freeit/crazytraining/settings/SettingsFragment.kt
+++ app/src/main/java/ru/freeit/crazytraining/settings/SettingsFragment.kt
@@ -12,6 +12,7 @@ import ru.freeit.crazytraining.core.theming.extensions.layoutParams
 import ru.freeit.crazytraining.core.theming.extensions.linearLayoutParams
 import ru.freeit.crazytraining.core.theming.extensions.padding
 import ru.freeit.crazytraining.core.theming.layout.components.CoreLinearLayout
+import ru.freeit.crazytraining.core.theming.view.CaptionTextView
 import ru.freeit.crazytraining.settings.view.ThemeSwitchView
 
 class SettingsFragment : BaseFragment() {
@@ -23,13 +24,21 @@ class SettingsFragment : BaseFragment() {
 
         changeTitle(getString(R.string.settings))
 
+        val themeCaptionView = CaptionTextView(context)
+        themeCaptionView.setText(R.string.select_theme)
+        themeCaptionView.layoutParams(linearLayoutParams().matchWidth().wrapHeight())
+        contentView.addView(themeCaptionView)
+
         val themeManager = (context.applicationContext as App).themeManager
         val themeSwitchView = ThemeSwitchView(context)
-        themeSwitchView.changeThemeSelectListener { theme ->
-            themeManager.changeTheme(theme)
-        }
-        themeSwitchView.layoutParams(linearLayoutParams().width(360).height(150))
+        themeSwitchView.changeThemeSelectListener { theme -> themeManager.changeTheme(theme) }
+        themeSwitchView.layoutParams(linearLayoutParams().width(context.dp(120)).height(context.dp(56)).marginTop(context.dp(8)))
         contentView.addView(themeSwitchView)
+
+        val daysCaptionView = CaptionTextView(context)
+        daysCaptionView.setText(R.string.select_training_days)
+        daysCaptionView.layoutParams(linearLayoutParams().matchWidth().wrapHeight().marginTop(context.dp(16)))
+        contentView.addView(daysCaptionView)
 
         return contentView
     }
